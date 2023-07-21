e33fea36013afe779d1c3455136cb4a40702468a
sjhong98 <sjhong98@icloud.com>
2023-07-21 15:13:53 +0900

    feat: kakao login

--- client/src/login.js
+++ client/src/login.js
@@ -1,10 +1,10 @@
 import React, {useContext, useState, useEffect} from 'react';
 import {Link, useLocation, useNavigate} from 'react-router-dom';
 
-const Login = ()=>
+const Login = () =>
 {
     const Rest_api_key='c105c81a4b71d2b2b6eb5313272815ef' //REST API KEY
-    const redirect_uri = 'http://localhost:3000' //Redirect URI
+    const redirect_uri = 'http://localhost:3000/login/auth' //Redirect URI
     // oauth 요청 URL
     const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`
     const handleLogin = ()=>{
