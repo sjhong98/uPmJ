54fe70b48f0b168d9bef03d538e88d8d8028b77e
taptap-kor <tyy8282@gmail.com>
2023-07-21 14:15:16 +0900

    update

---
+++ server/controller/user.controller.js
@@ -0,0 +1,20 @@
+const User = require("../sequelize/user.model");
+const express = require("express");
+const axios = require("axios");
+const app = express();
+
+const userCheck = () => {
+
+}
+
+const signUp = () => {
+
+}
+
+const createGroup = () => {
+
+}
+
+const joinGroup = () => {
+  
+}
