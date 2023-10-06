cb4ce0bb041b62ccf67f549bdbf679faf79cd2ab
taptap-kor <tyy8282@gmail.com>
2023-10-06 17:55:13 +0900

    feat: get plan list

--- server/routes/group.route.js
+++ server/routes/group.route.js
@@ -4,6 +4,7 @@ const controller = require("../controller/group.controller");
 router.post("/", controller.createGroup);
 router.post("/participants", controller.joinGroup);
 router.post("/info", controller.groupInfoFind);
+router.post("/plans", controller.getGroupPlans);
 
 module.exports = router;
 
